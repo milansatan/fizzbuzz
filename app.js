4e8a363953c98c86dc9a8181021c7886066a52f3
John Asabushi <milansatan@users.noreply.github.com>
2014-11-28 22:05:23 -0800

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ $(document).ready(function(){
 
 	function numList(){
 		var num = +$('#number').val();
-		var aNums = [];
-		var string = "";
+
 		for(var i = 1; i <= num; i++){
 			if(i % 3 == 0 || i % 5 == 0){
 				if(i % 3 ==0 && i % 5 != 0){
